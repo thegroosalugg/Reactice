1b604d1a6e75c20db80cfb0eb9a7ad51b1ff64d6
victor loginov <tkoe88@zoho.com>
2024-02-08 23:02:40 +0100

    wrapped memo around isPrime function

--- 10-ReactOptimisation/src/components/Counter/Counter.jsx
+++ 10-ReactOptimisation/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, memo, useCallback } from "react";
+import { useState, memo, useCallback, useMemo } from "react";
 
 import IconButton from "../UI/IconButton.jsx";
 import MinusIcon from "../UI/Icons/MinusIcon.jsx";
@@ -28,7 +28,10 @@ function isPrime(number) {
 // memo should only be used on a component that is as high up in the component tree as possible. If the parent component does not execute, nor do its children
 const Counter = memo(function Counter({ initialCount }) { // memo is no longer needed here due to state uplifting, but has been kept for learning reference
   log("<Counter /> rendered", 1);
-  const initialCountIsPrime = isPrime(initialCount);
+
+  // useMemo, like memo wraps a normal JS function as opposed to a component function above
+  // we pass a function, i.e. an anonymous arrow function, which then returns the result of the function that we want to prevent executing
+  const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]) // the dependency is the prop the function relies on, if the prop changes then the function re-executes
 
   const [counter, setCounter] = useState(initialCount);
 
