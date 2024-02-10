2f72a4db7590e6bdd066ac0cd24c767c8dc49cac
victor loginov <tkoe88@zoho.com>
2024-02-10 15:55:08 +0100

    added updated counter file. Implemented counterhistory with objects instead of index

--- 10-ReactOptimisation/src/components/Counter/Counter.jsx
+++ 10-ReactOptimisation/src/components/Counter/Counter.jsx
@@ -5,9 +5,11 @@ import MinusIcon from "../UI/Icons/MinusIcon.jsx";
 import PlusIcon from "../UI/Icons/PlusIcon.jsx";
 import CounterOutput from "./CounterOutput.jsx";
 import { log } from "../../log.js";
+import CounterHistory from "./CounterHistory.jsx";
 
 function isPrime(number) {
   log("Calculating if is prime number", 2, "other");
+
   if (number <= 1) {
     return false;
   }
@@ -23,26 +25,39 @@ function isPrime(number) {
   return true;
 }
 
-// memo is a react hook which will compare the old & new prop values, and if they are exactly the same React will not execute this component again
-// memo is used to optimise app performance as unrelated state changes which re-execute the parent App component also re-execute all the child components
-// memo should only be used on a component that is as high up in the component tree as possible. If the parent component does not execute, nor do its children
-const Counter = memo(function Counter({ initialCount }) { // memo is no longer needed here due to state uplifting, but has been kept for learning reference
+const Counter = memo(function Counter({ initialCount }) {
   log("<Counter /> rendered", 1);
 
-  // useMemo, like memo wraps a normal JS function as opposed to a component function above
-  // we pass a function, i.e. an anonymous arrow function, which then returns the result of the function that we want to prevent executing
-  const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]) // the dependency is the prop the function relies on, if the prop changes then the function re-executes
+  const initialCountIsPrime = useMemo(
+    () => isPrime(initialCount),
+    [initialCount]
+  );
 
-  const [counter, setCounter] = useState(initialCount);
+  // const [counter, setCounter] = useState(initialCount);
+  const [counterChanges, setCounterChanges] = useState([
+    { val: initialCount, id: Math.random() * 1000 },
+  ]);
 
-  // all functions are JS objects, they're rendered each time the App is, and create a new object
-  const handleDecrement = useCallback(function handleDecrement() { // useCallback stops these functions from being executed each time the app is rendered
-    setCounter((prevCounter) => prevCounter - 1); // re-rended function prevents memo from inside IconButton from working, callback prevents this function from recreation
-  }, [])  // empty array of dependencies which will re-execute the function when app is rendered.
+  const currentCounter = counterChanges.reduce(
+    (prevCounter, counterChange) => prevCounter + counterChange.val,
+    0
+  );
+
+  const handleDecrement = useCallback(function handleDecrement() {
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [
+      { val: -1, id: Math.random() * 1000 },
+      ...prevCounterChanges,
+    ]);
+  }, []);
 
   const handleIncrement = useCallback(function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
-  }, [])
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [
+      { val: 1, id: Math.random() * 1000 },
+      ...prevCounterChanges,
+    ]);
+  }, []);
 
   return (
     <section className="counter">
@@ -54,13 +69,14 @@ const Counter = memo(function Counter({ initialCount }) { // memo is no longer n
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
           Decrement
         </IconButton>
-        <CounterOutput value={counter} />
+        <CounterOutput value={currentCounter} />
         <IconButton icon={PlusIcon} onClick={handleIncrement}>
           Increment
         </IconButton>
       </p>
+      <CounterHistory history={counterChanges} />
     </section>
   );
 });
 
-export default Counter
+export default Counter;
