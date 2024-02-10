364b15c21d21af9bf4889c5f3544b824c60a7563
victor loginov <tkoe88@zoho.com>
2024-02-10 21:25:07 +0100

    added key to unmount/remount component

--- 10-ReactOptimisation/src/App.jsx
+++ 10-ReactOptimisation/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Counter from './components/Counter/Counter.jsx';
-import Header from './components/Header.jsx';
-import { log } from './log.js';
-import Configurator from './components/Counter/Configurator.jsx';
+import Counter from "./components/Counter/Counter.jsx";
+import Header from "./components/Header.jsx";
+import { log } from "./log.js";
+import Configurator from "./components/Counter/Configurator.jsx";
 
 function App() {
-  log('<App /> rendered');
+  log("<App /> rendered");
 
   const [chosenCount, setChosenCount] = useState(0);
 
   function handleSetCount(newCount) {
-    setChosenCount(newCount)
+    setChosenCount(newCount);
+    setChosenCount((prevCount) => prevCount + 1); // React does not update the state instantly within the same function, and as such new values will not be displayed
+    // instead an arrow function must be used in order to update the new value within the same function
+    console.log(chosenCount); // this would not work. It will display the previous state, not the latest that has been updated
   }
 
   return (
@@ -19,7 +22,8 @@ function App() {
       <Header />
       <main>
         <Configurator onSet={handleSetCount} />
-        <Counter initialCount={chosenCount} />
+        {/* Key is not only for list items, it can be used on any component to unmount/remount when the value of the key changes */}
+        <Counter key={chosenCount} initialCount={chosenCount} />
       </main>
     </>
   );
