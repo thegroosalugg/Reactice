94efc731b283a0a3f9468afb0f8247ca951cb54a
victor loginov <tkoe88@zoho.com>
2024-02-11 13:01:47 +0100

    converted user component to class

--- 11-ClassBasedComponents/src/components/User.js
+++ 11-ClassBasedComponents/src/components/User.js
@@ -1,7 +1,18 @@
-import classes from './User.module.css';
+import classes from "./User.module.css";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// class based components require this import to access props
+import { Component } from "react";
+
+// component is then extended from class
+class User extends Component {
+  render() { // all class based components must be rendered with this built-in JS function
+    return <li className={classes.user}>{this.props.name}</li>; // to access props, we must access this.props
+  }
+}
+
+// this is function component is the same as the class based component above
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
