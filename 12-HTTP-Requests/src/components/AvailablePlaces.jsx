3e9467e73a3b9e329931dba9f812e3dc9bfd2362
victor loginov <tkoe88@zoho.com>
2024-02-13 00:09:27 +0100

    set up fetch requests to get avail places from the backend

--- 12-HTTP-Requests/src/components/AvailablePlaces.jsx
+++ 12-HTTP-Requests/src/components/AvailablePlaces.jsx
@@ -1,10 +1,24 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [avalablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places") // Sending a GET request to the specified URL
+      .then((response) => {               // Handling the response from the fetch request
+        return response.json();           // Parsing the response body as JSON
+      })
+      .then((resData) => {                  // Handling the JSON data extracted from the response
+        setAvailablePlaces(resData.places); // Updating the state with the fetched places data
+      });
+  }, []); // Dependency array is empty, indicating this effect runs only once on component mount
+
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={avalablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
