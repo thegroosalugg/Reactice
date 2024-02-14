8243cf4ab25dfa93b0f27bb6948b13cc30222601
victor loginov <tkoe88@zoho.com>
2024-02-14 21:45:04 +0100

    await async function created for available places

--- 12-HTTP-Requests/src/components/AvailablePlaces.jsx
+++ 12-HTTP-Requests/src/components/AvailablePlaces.jsx
@@ -5,15 +5,25 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [avalablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places") // Sending a GET request to the specified URL
-      .then((response) => {               // Handling the response from the fetch request
-        return response.json();           // Parsing the response body as JSON
-      })
-      .then((resData) => {                  // Handling the JSON data extracted from the response
-        setAvailablePlaces(resData.places); // Updating the state with the fetched places data
-      });
-  }, []); // Dependency array is empty, indicating this effect runs only once on component mount
+    async function fetchPlaces() { // this is the correct way to use async/await in React's useEffect
+      const response = await fetch("http://localhost:3000/places"); // the code is more readable than the one below
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
 
+    fetchPlaces();
+  }, []);
+
+  // original method pre-async
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/places") // Sending a GET request to the specified URL
+  //     .then((response) => {               // Handling the response from the fetch request
+  //       return response.json();           // Parsing the response body as JSON
+  //     })
+  //     .then((resData) => {                  // Handling the JSON data extracted from the response
+  //       setAvailablePlaces(resData.places); // Updating the state with the fetched places data
+  //     });
+  // }, []); // Dependency array is empty, indicating this effect runs only once on component mount
 
   return (
     <Places
