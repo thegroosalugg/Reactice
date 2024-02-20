d3c1fc4777caa949ae247bb56d7b0225827326fa
victor loginov <tkoe88@zoho.com>
2024-02-20 16:42:15 +0100

    aadded submission based email validation to ref login

--- 14-Forms&UserInput/src/components/RefLogin.jsx
+++ 14-Forms&UserInput/src/components/RefLogin.jsx
@@ -1,42 +1,47 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function RefLogin() {
+  const [emailInvalid, setEmailInvalid] = useState(false);
   const email = useRef();
   const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault(); // prevents browser's default behaviour, i.e. submitting form and sending an HTTP request
 
-    const userEmail = email.current.value
-    const userPassword = password.current.value
+    const userEmail = email.current.value;
+    const userPassword = password.current.value;
 
+    const emailValid = userEmail.includes("@");
+
+    if (!emailValid) {
+      setEmailInvalid(true);
+      return; // return ensures no other code in this function is executed if the IF check is passed
+    }
+
+    setEmailInvalid(false); // remove error message if submitted form skips IF check
+
+    console.log('Sending HTTP request');
     console.log(userEmail, userPassword);
   }
 
   return (
     // alternative method to handle button submitting is to add the event listener to the form instead of the button
-    <form onSubmit={handleSubmit}>
+    // noValidate disables browser's built in validations. Since that is what we're working on, they are interferring
+    <form noValidate onSubmit={handleSubmit}>
       <h2>Ref Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            ref={email}
-          />
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailInvalid && <p>Enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            ref={password}
-          />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
