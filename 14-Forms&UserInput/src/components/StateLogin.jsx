e4e2609eac0128818ce8604777f68f0d714c7421
victor loginov <tkoe88@zoho.com>
2024-02-20 16:12:31 +0100

    added blur(focus) based email validation

--- 14-Forms&UserInput/src/components/StateLogin.jsx
+++ 14-Forms&UserInput/src/components/StateLogin.jsx
@@ -2,15 +2,23 @@ import { useState } from "react";
 
 export default function StateLogin() {
   const [userData, setUserData] = useState({ email: "", password: "" });
+  const [isEditting, setIsEditting] = useState({ email: false, password: false });
+
+  const emailInvalid = isEditting.email && !userData.email.includes('@');
 
   function handleSubmit(event) {
     event.preventDefault(); // prevents browser's default behaviour, i.e. submitting form and sending an HTTP request
     console.log(userData);
   }
 
+  function handleBlur(dataType) {
+    setIsEditting((prevState) => ({ ...prevState, [dataType]: true}))
+  }
+
   function handleInputChange(dataType, value) {
     // when the function is passed, we wrap the curlies {} in parenthesis () to tell React we're creating an Object and not starting a function
     setUserData((prevData) => ({ ...prevData, [dataType]: value })); // using the [] syntax we can directly access a key inside the object and then change its values
+    setIsEditting((prevState) => ({ ...prevState, [dataType]: false})) // revert blur state to false to remove error messages until user finishes editting
   }
 
   return (
@@ -25,9 +33,11 @@ export default function StateLogin() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleBlur('email')} // onBlur is an HTML prop which activates when the element loses focus
             onChange={(event) => handleInputChange("email", event.target.value)} // e-t-v captures the user input data and passes its value as an argument
             value={userData.email}
           />
+          <div className="control-error">{emailInvalid && <p>Enter a valid email address</p>}</div>
         </div>
 
         <div className="control no-margin">
@@ -36,6 +46,7 @@ export default function StateLogin() {
             id="password"
             type="password"
             name="password"
+            onBlur={() => handleBlur('password')}
             onChange={(event) => handleInputChange("password", event.target.value)}
             value={userData.password}
           />
