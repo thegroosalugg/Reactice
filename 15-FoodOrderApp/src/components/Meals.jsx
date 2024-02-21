2df5999c79c7d9d9e371d7835e65d7b9ef7b7961
victor loginov <tkoe88@zoho.com>
2024-02-21 19:09:13 +0100

    meal component constructed as per provided CSS code. All backend data is now displayed

--- 15-FoodOrderApp/src/components/Meals.jsx
+++ 15-FoodOrderApp/src/components/Meals.jsx
@@ -1,7 +1,21 @@
 export default function Meals({ meals }) {
-  return <section id="">
-    <ul className="">
-      {meals.map((meal) => <li key={meal.id}>{meal.name}</li>)}
+  return (
+    <ul id="meals">
+      {meals.map((meal) => (
+        <li key={meal.id} className="meal-item">
+          <article>
+            <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+            <div>
+              <h3>{meal.name}</h3>
+              <p className="meal-item-price">${meal.price}</p>
+              <p className="meal-item-description">{meal.description}</p>
+            </div>
+            <p className="meal-item-actions">
+              <button className="button">Add to Card</button>
+            </p>
+          </article>
+        </li>
+      ))}
     </ul>
-  </section>
+  );
 }
