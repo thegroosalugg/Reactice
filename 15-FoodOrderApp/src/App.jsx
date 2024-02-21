be1b6e8c67e817d9b5d3d819e0ea8c262adf2033
victor loginov <tkoe88@zoho.com>
2024-02-21 21:49:02 +0100

    created Modal component and connected it to App

--- 15-FoodOrderApp/src/App.jsx
+++ 15-FoodOrderApp/src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+import Modal from "./components/Modal";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 
 function App() {
+  const [cartState, setCartState] = useState([]);
+  const [modalState, setModalState] = useState(false);
+
+  function openModal() {
+    setModalState(true);
+  }
+
+  function closeModal() {
+    setModalState(false);
+  }
+
   return (
     <>
-      <Header />
+      <Modal open={modalState} closeModal={closeModal}>
+        {cartState.length <= 0 && <h2>Your Cart is Empty</h2>}
+      </Modal>
+      <Header openCart={openModal} />
       <Menu />
     </>
   );
--- 15-FoodOrderApp/src/components/Header.jsx
+++ 15-FoodOrderApp/src/components/Header.jsx
@@ -1,14 +1,13 @@
-import logo from "../assets/logo.jpg"
+import logo from "../assets/logo.jpg";
 
-export default function Header() {
-
+export default function Header({ openCart }) {
   return (
     <header id="main-header">
       <div id="title">
         <img src={logo} alt="logo" />
         <h1>Laugh and grow fat</h1>
       </div>
-      <button className="text-button">Cart</button>
+      <button className="text-button" onClick={openCart}>Cart</button>
     </header>
   );
 }
