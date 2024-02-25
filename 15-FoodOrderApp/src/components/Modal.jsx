e0074353386276994e5750182ff00721cb7865c9
Victor Loginov <138363579+thegroosalugg@users.noreply.github.com>
2024-02-25 18:36:54 +0100

    Update Modal.jsx
    no argument required

--- 15-FoodOrderApp/src/components/Modal.jsx
+++ 15-FoodOrderApp/src/components/Modal.jsx
@@ -15,7 +15,7 @@ export default function Modal({ open, closeModal, children }) {
   return createPortal(
     // onClose is a built-in listener that will execute a function if the dialog is closed with ESC instead of button
     // this is necessary to reset the modal state to false, otherwise its impossible to reopen modal
-    <dialog className="modal" ref={dialog} onClose={() => closeModal(false)}>
+    <dialog className="modal" ref={dialog} onClose={() => closeModal()}>
       {open ? children : null}
     </dialog>,
     document.getElementById("modal")
