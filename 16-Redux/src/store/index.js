23241ab0a3b940a1dcb75ad9541793c078b59f08
victor loginov <tkoe88@zoho.com>
2024-02-26 17:22:39 +0100

    added toggle display counter function to reducer for both, functional and class based components

--- 16-Redux/src/store/index.js
+++ 16-Redux/src/store/index.js
@@ -3,9 +3,14 @@ import { createStore } from "redux";
 // redux store file. May be marked as depreciated but can still be used
 
 // create a reducer function like in React. The state is given an initial value
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "update") { // when this 'type" of action is called, it expects to get an 'amount' and changes the state by that amount
-    return { counter: state.counter + action.amount };
+const counterReducer = (state = { counter: 0, display: true }, action) => {
+  if (action.type === "update") {
+    // when this 'type" of action is called, it expects to get an 'amount' and changes the state by that amount
+    return { ...state, counter: state.counter + action.amount };
+  }
+
+  if (action.type === "toggle") {
+    return { ...state, display: !state.display }; // change true to false, false to true
   }
 
   return state;
