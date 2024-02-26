f9d62fc0b7286d0612727d2053b145c848b66140
victor loginov <tkoe88@zoho.com>
2024-02-26 19:43:25 +0100

    redux toolkit installed. Store reconfigured with createSlice. Configured Counter

--- 16-Redux/src/store/index.js
+++ 16-Redux/src/store/index.js
@@ -1,22 +1,47 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 // redux store file. May be marked as depreciated but can still be used
 
-// create a reducer function like in React. The state is given an initial value
-const counterReducer = (state = { counter: 0, display: true }, action) => {
-  if (action.type === "update") {
-    // when this 'type" of action is called, it expects to get an 'amount' and changes the state by that amount
-    return { ...state, counter: state.counter + action.amount };
-  }
+const initialState = { count: 0, display: true };
 
-  if (action.type === "toggle") {
-    return { ...state, display: !state.display }; // change true to false, false to true
-  }
+// createSlice from Redux Toolkit
+const counterSlice = createSlice({
+  name: "counter", // name required
+  initialState,
+  reducers: {
+    // in create slice, we can manipulate the state directly as a copy will be made to ensure it remains inmutable
+    update(state, action) {
+      state.count += action.payload; // redux sets the value data we obtain as payload, it cannot be renamed
+    },
+    toggle(state) {
+      state.display = !state.display;
+    },
+  },
+});
 
-  return state;
-};
-
-// redux has only one store. It is then pointing at the function above
-const store = createStore(counterReducer);
-
+export const counterActions = counterSlice.actions
+// configureStore expects an object with a reducer key. In a large component store you store an object with your other reducers inside
+// store states selected based on keys we name them, so in this store "state.counter.count" || "state.counter.display" || "state.counter.update()"
+const store = configureStore({ reducer: { counter: counterSlice.reducer } });
 export default store;
+
+// original redux-react reducer
+// create a reducer function like in React. The state is given an initial value
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "update") {
+//     // when this 'type" of action is called, it expects to get an 'amount' and changes the state by that amount
+//     return { ...state, counter: state.counter + action.amount };
+//   }
+
+//   if (action.type === "toggle") {
+//     return { ...state, display: !state.display }; // change true to false, false to true
+//   }
+
+//   return state;
+// };
+
+// // redux has only one store. It is then pointing at the function above
+// const store = createStore(counterReducer);
+
+// export default store;
