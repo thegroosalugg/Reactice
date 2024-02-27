7221221d793d02f7a2b8cf64129929a881818465
victor loginov <tkoe88@zoho.com>
2024-02-27 14:11:44 +0100

    created some dummy products and output them on main page

--- 17-AdvancedRedux/src/components/Cart/Cart.js
+++ 17-AdvancedRedux/src/components/Cart/Cart.js
@@ -1,20 +1,24 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
-import { cartActions } from '../../store/cartReducer';
-import { useDispatch } from 'react-redux';
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+import { cartActions } from "../../store/cartReducer";
+import { useDispatch, useSelector } from "react-redux";
 
 const Cart = (props) => {
   const dispatch = useDispatch();
+  const items = useSelector((state) => state.items);
 
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
-      <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
-      </ul>
+      {items.length <= 0 && <p>Cart is Empty. Buy more stuff</p>}
+      {items.length > 0 && (
+        <ul>
+          {items.map((item) => (
+            <CartItem {...item} />
+          ))}
+        </ul>
+      )}
       <button onClick={() => dispatch(cartActions.show(false))}>Close</button>
     </Card>
   );
---
+++ 17-AdvancedRedux/src/components/Data/availableProducts.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: "p1",
+    title: "Plum",
+    price: 6.9,
+    description: "You're plum if you don't buy it!",
+  },
+  {
+    id: "p2",
+    title: "Banana",
+    price: 3.65,
+    description: "Bananas go well with Beer",
+  },
+  {
+    id: "p3",
+    title: "Beer",
+    price: 2.34,
+    description: "Eat some bread",
+  },
+  {
+    id: "p4",
+    title: "Bread",
+    price: 4.32,
+    description: "Wouldn't be complete without a plum",
+  },
+];
+
+export default products;
