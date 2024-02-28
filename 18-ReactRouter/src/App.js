fdac74b5f7c07e64a80bb99b35d75221066cf2c4
victor loginov <tkoe88@zoho.com>
2024-02-28 20:57:13 +0100

    created router and routed 2 dummy page components

--- 18-ReactRouter/src/App.js
+++ 18-ReactRouter/src/App.js
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Products from "./pages/Products";
+
+const router = createBrowserRouter([
+  {
+    path: "/", // homepage path is empty. Specific pages come after /
+    element: <Homepage />, // element set to the imported component
+  },
+  { path: "/products", element: <Products /> },
+]);
+
 function App() {
-  return <div></div>;
+  // return only RouterProvider which has a router prop, set to the created BrowserRouter
+  return <RouterProvider router={router} />;
 }
 
 export default App;
