a2d013120d1b099cc1988c82107731ee4e4a9745
victor loginov <tkoe88@zoho.com>
2024-02-29 13:41:59 +0100

    added dummy events to render

--- 19-AdvancedRouting/frontend/src/pages/EventsPage.js
+++ 19-AdvancedRouting/frontend/src/pages/EventsPage.js
@@ -1,7 +1,14 @@
-export default function EventsPage() {
-  return (
-    <div>
-      <h2>Events</h2>
-    </div>
-  );
+import EventsList from "../components/EventsList";
+
+const EVENTS = [
+  { title: "Jumping", date: "22-01-2024", image: "" },
+  { title: "Swimming", date: "12-04-2028", image: "" },
+  { title: "Crouching", date: "22-01-2024", image: "" },
+  { title: "Running", date: "29-10-2034", image: "" }
+];
+
+function EventsPage() {
+  return <EventsList events={EVENTS} />;
 }
+
+export default EventsPage;
