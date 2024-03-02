5b177d1003c817ea697c600cbe505f064859312c
victor loginov <tkoe88@zoho.com>
2024-03-02 22:06:42 +0100

    can delete events

--- 19-AdvancedRouting/frontend/src/components/EventItem.js
+++ 19-AdvancedRouting/frontend/src/components/EventItem.js
@@ -1,15 +1,20 @@
 import classes from "./EventItem.module.css";
-import { useParams, json, Link, useRouteLoaderData } from "react-router-dom";
+import { useParams, json, Link, useRouteLoaderData, redirect, useSubmit } from "react-router-dom";
 
 export default function EventItem() {
   const { eventIdFromRouter } = useParams();
   // const { event } = useLoaderData(); // response data is an object with an event key
   const { event } = useRouteLoaderData("event-info") // useLoaderData searches only current route, when accessing parent data, we useRouteLoaderData
+  const submit = useSubmit();
 
   console.log("Child:", event);
 
   function startDeleteHandler() {
-    // ...
+    const goForth = window.confirm("Are you positive?") // built-in browser confirmation pop up
+
+    if (goForth) { // first argument is data to be submitted, in this case, not required
+      submit(null, { method: 'delete' }); // method sent to deleteEvent function for configuration
+    }
   }
 
   return (
@@ -46,4 +51,17 @@ export async function loadEvent({ request, params }) {
       console.log(response)
       return response;
     }
+}
+
+export async function deleteEvent({ request, params }) {
+  const id = params.eventIdFromRouter;
+
+  const response = await fetch("http://localhost:8080/events/" + id, { method: request.method }); // configuration extracts method from useSubmit above
+
+  if (!response.ok) {
+    throw json(
+      { title: "Undeletable", message: "The file you tried to erase grows stronger" }, { status: 500 }
+      );
+    }
+    return redirect("/events")
 }
