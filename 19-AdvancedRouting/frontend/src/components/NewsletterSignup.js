df0fd9041255bdb72bf416dd4d5dabceae1e941e
victor loginov <tkoe88@zoho.com>
2024-03-03 15:03:37 +0100

    added useFetcher to newsletterSignUp to trigger actions on form

--- 19-AdvancedRouting/frontend/src/components/NewsletterSignup.js
+++ 19-AdvancedRouting/frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,28 @@
+import { useFetcher } from 'react-router-dom';
 import classes from './NewsletterSignup.module.css';
+import { useEffect } from 'react';
 
 function NewsletterSignup() {
+  const fetcher = useFetcher(); // React-Router hook object with several built-in functions, often used for triggering actions without route transitions
+  const { state, data } = fetcher // extract other properties from fetcher. state: "idle" | "loading" | "submitting"
+
+  useEffect(() => {
+    if (state === "idle" && data && data.message) {
+      window.alert(data.message) // retrieve message from NewsletterPage signUp action function
+    }
+  }, [state, data]) // will re-execute with any state or data changes
+
   return (
-    <form method="post" className={classes.newsletter}>
+    // this form is included on all routes as its used in the NavBar, therefore using React-Router's Form hook would mean placing the action on every route
+    // fetcher's form will trigger an action without initialising a route transition
+    <fetcher.Form method="post" action="/newsletter" className={classes.newsletter}>
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
