b3482d0a78b28a914434b0fad55f2838206e4b5e
victor loginov <tkoe88@zoho.com>
2024-03-09 21:11:21 +0100

    added http sending, pending & error handling queries to new event form

--- 22-Tanstack/src/components/Events/NewEvent.jsx
+++ 22-Tanstack/src/components/Events/NewEvent.jsx
@@ -1,25 +1,42 @@
 import { Link, useNavigate } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { createNewEvent } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
   const navigate = useNavigate();
+  const { mutate, isError, error, isPending } = useMutation({
+    mutationFn: createNewEvent,
+  }); // similar to useQuery, but used to send http requests, such as updating/creating/deleting
 
-  function handleSubmit(formData) {}
+  function handleSubmit(formData) {
+    mutate({ events: formData });
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && 'Submitting...'}
+        {!isPending && (
+          <>
+            <Link to='../' className='button-text'>
+              Cancel
+            </Link>
+            <button type='submit' className='button'>
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title='Failed to create event'
+          message={error.info?.message || "Check your input"}
+        />
+      )}
     </Modal>
   );
 }
