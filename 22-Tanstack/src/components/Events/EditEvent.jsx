af848d978b1d335542e2aac06c50c20269738271
victor loginov <tkoe88@zoho.com>
2024-03-10 17:06:46 +0100

    updated edit event pending & error UI

--- 22-Tanstack/src/components/Events/EditEvent.jsx
+++ 22-Tanstack/src/components/Events/EditEvent.jsx
@@ -1,10 +1,19 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvent } from '../../util/http.js';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { id } = useParams();
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', id], // same key as in Event Details means the form will be filled with data instantly thanks to caching
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+  });
 
   function handleSubmit(formData) {}
 
@@ -12,16 +21,42 @@ export default function EditEvent() {
     navigate('../');
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
+  let content;
+
+  if (isPending) {
+    content = (
+      <div className='center'>
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <>
+        <ErrorBlock
+          title='Loading failed'
+          message={error.info?.message || "Couldn't fetch event details"}
+        />
+        <div className='form-actions'>
+          <Link to='../' className='button'>Close</Link>
+        </div>
+      </>
+    );
+  }
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
+        <Link to='../' className='button-text'>
           Cancel
         </Link>
-        <button type="submit" className="button">
+        <button type='submit' className='button'>
           Update
         </button>
       </EventForm>
-    </Modal>
-  );
+    );
+  }
+
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
