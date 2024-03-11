4bc514c3609ebcea1a6e20c1a6ab45a40ef10c91
victor loginov <tkoe88@zoho.com>
2024-03-11 18:54:31 +0100

    swapped app from using react query to router on event edit

--- 22-Tanstack/src/App.jsx
+++ 22-Tanstack/src/App.jsx
@@ -8,7 +8,7 @@ import { QueryClientProvider } from '@tanstack/react-query'; // required to use 
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
-import EditEvent from './components/Events/EditEvent.jsx';
+import EditEvent, { action as editEventAction, loader as editEventsLoader} from './components/Events/EditEvent.jsx';
 import { queryClient } from './util/http.js';
 
 const router = createBrowserRouter([
@@ -34,6 +34,8 @@ const router = createBrowserRouter([
       {
         path: '/events/:id/edit',
         element: <EditEvent />,
+        loader: editEventsLoader, // totally optional. Router approach to loading data. Can just use Query instead
+        action: editEventAction,
       },
     ],
   },
