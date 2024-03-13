3dddd42516ceeac166f38825094d94a22238204f
victor loginov <tkoe88@zoho.com>
2024-03-13 18:01:54 +0100

    outsourced header background to separate component

---
+++ 24-NextFood/components/main-header/main-header.jsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import css from './main-header.module.css';
+
+import logo from '@/assets/logo.png';
+import Image from 'next/image';
+import HeaderBackground from './header-background';
+
+export default function MainHeader() {
+  return (
+    <>
+      <HeaderBackground />
+      <header className={css.header}>
+        <Link href='/' className={css.logo}>
+          {/* in NextJS the image is an object and we must acess its src property */}
+          {/* <img src={logo.src} alt='munch' /> */}
+          {/* Using the alternative Next Image hook. Pass the whole object instead of a key. Adds extra properties. Lazy Loading.
+        'Priority' sets loading priority, as image will always be displayed */}
+          <Image src={logo} alt='munch' priority />
+          Let's Go Home
+        </Link>
+        <nav className={css.nav}>
+          <ul>
+            <li>
+              <Link href='/meals'>Meals</Link>
+            </li>
+            <li>
+              <Link href='/community'>Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
