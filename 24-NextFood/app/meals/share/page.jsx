5da13217356758b8b52334b4b47c95d99c28ed42
victor loginov <tkoe88@zoho.com>
2024-03-16 11:01:07 +0100

    added predefined meal share pages

--- 24-NextFood/app/meals/share/page.jsx
+++ 24-NextFood/app/meals/share/page.jsx
@@ -1,12 +1,49 @@
-import Link from "next/link";
+import css from './page.module.css';
 
-export default function MealShare() {
+export default function ShareMealPage() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Share
-      </h1>
-      <p><Link href='/meals'>Back to Meals</Link></p>
-    </main>
+    <>
+      <header className={css.header}>
+        <h1>
+          Share your <span className={css.highlight}>favorite meal</span>
+        </h1>
+        <p>Or any other meal you feel needs sharing!</p>
+      </header>
+      <main className={css.main}>
+        <form className={css.form}>
+          <div className={css.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              required
+            ></textarea>
+          </p>
+          IMAGE PICKER
+          <p className={css.actions}>
+            <button type="submit">Share Meal</button>
+          </p>
+        </form>
+      </main>
+    </>
   );
 }
