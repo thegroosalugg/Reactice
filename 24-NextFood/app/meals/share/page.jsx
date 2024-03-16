00b82b1a67afa6cfa3f6c5dfd9383b99129a31b1
victor loginov <tkoe88@zoho.com>
2024-03-16 13:14:07 +0100

    capture and submit form data to server

--- 24-NextFood/app/meals/share/page.jsx
+++ 24-NextFood/app/meals/share/page.jsx
@@ -2,6 +2,21 @@ import ImagePicker from '@/components/meals/image-picker';
 import css from './page.module.css';
 
 export default function ShareMealPage() {
+  async function shareMeal(formData) {
+    'use server'; // special directive that ensures the function executes on the server and not the client. Must be async
+
+    const meal = {
+      title: formData.get('title'),
+      image: formData.get('image'),
+      summary: formData.get('summary'),
+      instructions: formData.get('instructions'),
+      creator: formData.get('name'),
+      creator_email: formData.get('email'),
+    }
+
+    console.log(meal)
+  }
+
   return (
     <>
       <header className={css.header}>
@@ -11,37 +26,38 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={css.main}>
-        <form className={css.form}>
+        {/* attach server functions via action */}
+        <form className={css.form} action={shareMeal}>
           <div className={css.row}>
             <p>
-              <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
+              <label htmlFor='name'>Your name</label>
+              <input type='text' id='name' name='name' required />
             </p>
             <p>
-              <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
+              <label htmlFor='email'>Your email</label>
+              <input type='email' id='email' name='email' required />
             </p>
           </div>
           <p>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" required />
+            <label htmlFor='title'>Title</label>
+            <input type='text' id='title' name='title' required />
           </p>
           <p>
-            <label htmlFor="summary">Short Summary</label>
-            <input type="text" id="summary" name="summary" required />
+            <label htmlFor='summary'>Short Summary</label>
+            <input type='text' id='summary' name='summary' required />
           </p>
           <p>
-            <label htmlFor="instructions">Instructions</label>
+            <label htmlFor='instructions'>Instructions</label>
             <textarea
-              id="instructions"
-              name="instructions"
-              rows="10"
+              id='instructions'
+              name='instructions'
+              rows='10'
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker name='image' label="Current Pic" />
           <p className={css.actions}>
-            <button type="submit">Share Meal</button>
+            <button type='submit'>Share Meal</button>
           </p>
         </form>
       </main>
