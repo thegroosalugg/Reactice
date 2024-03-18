9693fd7331d878a155e3006a1c092b5413453da9
victor loginov <tkoe88@zoho.com>
2024-03-18 15:52:04 +0100

    deleted unnecessary bloat. added 2 starting routes

--- 25-NextPagesRouter/pages/_document.js
+++ 25-NextPagesRouter/pages/_document.js
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+// found in pages router next projects. Reserved file name in 'pages/_document.js'. Overwrites default Document
+
 export default function Document() {
   return (
     <Html lang="en">
---
+++ 25-NextPagesRouter/pages/news.js
@@ -0,0 +1,5 @@
+// custom created page. Is found on our-domain.com/news
+
+export default function NewsPage() {
+  return <h1>News</h1>;
+}
