31840e4d40d54fe07d08bc1ee8f403af66ffb3c4
victor loginov <tkoe88@zoho.com>
2024-03-19 20:48:13 +0100

    connected mongo db. Can post data

--- 26-NextMeetUp/pages/new-meetup/index.js
+++ 26-NextMeetUp/pages/new-meetup/index.js
@@ -1,11 +1,25 @@
-import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+import NewMeetupForm from '@/components/meetups/NewMeetupForm';
+import { useRouter } from 'next/router';
 
 // NeW Meet Up Folder. Root index page
 
 export default function NewMeetUpPage() {
-  function handleAddMeetUp(data) {
-    console.log(data)
+  const router = useRouter();
+
+  async function handleAddMeetUp(formData) {
+    // send to URL with external API, but here it is internal, so it takes API folder path/name of nested folder (same as current folder also)
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.replace('/'); // replace is like push, redirects, but ensures we cannot click back button
   }
 
-  return <NewMeetupForm onAddMeetup={handleAddMeetUp} />
+  return <NewMeetupForm onAddMeetup={handleAddMeetUp} />;
 }
