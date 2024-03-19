05344913867f05d829d07941efa9b158e22c1e94
victor loginov <tkoe88@zoho.com>
2024-03-19 21:48:16 +0100

    mongo data now fetched on homepage

--- 26-NextMeetUp/pages/index.js
+++ 26-NextMeetUp/pages/index.js
@@ -1,25 +1,7 @@
+import { MongoClient } from 'mongodb';
 import MeetupList from '@/components/meetups/MeetupList';
 
 // ROOT PAGE. It all starts here
-
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'Dummy',
-    image:
-      'https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?w=1060&t=st=1710785271~exp=1710785871~hmac=57df0597a583af72601c3aca89b7b9f7aa2d4f1f9d3d02c63775d79ed744f297',
-    address: '123 Fake Street',
-    description: 'Some dummy data',
-  },
-  {
-    id: 'm2',
-    title: 'Skeleton',
-    image:
-      'https://img.freepik.com/free-photo/snowy-mountain-peak-starry-galaxy-majesty-generative-ai_188544-9650.jpg?w=1060&t=st=1710785292~exp=1710785892~hmac=8f786deefb7628bdb570d9d184475554016ff40d68bcdfc69df96fd84f267830',
-    address: '1999 East',
-    description: 'Some skeletal data',
-  },
-];
 
 // props object is received directly from getStaticProps below, otherwise no data would have been passed
 export default function HomePage(props) {
@@ -29,12 +11,28 @@ export default function HomePage(props) {
 // Reserved function name. Pages Router component, obsolete in App Router. It calls this function before it calls the component.
 // contains the initial data this page needs. UseEffect runs AFTER the component renders once, thus initial data is missing, affecting SEO
 // executed during the build process, not on the server or client, so the code is protected. Creates a static page for initial render
+
 export async function getStaticProps() {
-  // fetch data for API
+  // same code as in new-meetups API, reused for visualisation
+  const client = await MongoClient.connect(process.env.MONGODB_URI); // fetched from ENV file
+  const db = client.db(); // get hold of connected db from the client
+  const meetupCollections = db.collection('meetups'); // name of 'collection' in Mongo
+
+  // find by default finds all the documents. ToArray transforms it as our component expects an array
+  const meetups = await meetupCollections.find().toArray();
+
+  client.close(); // close the connection to DB
 
   // must return an object with a PROPS key, this then holds another object with the data we want
   return {
-    props: { meetups: DUMMY_DATA },
+    props: { // fetched data must be transformed due to ID syntax used in Mongo
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        image: meetup.image,
+        address: meetup.address,
+        id: meetup._id.toString(), // Mongo has a '_id' key, and must be converted to string for it to work in our component
+      })),
+    },
     revalidate: 10, // number of seconds before NextJS regenerates this page for an incoming request
   }; // this ensures that when new data is added, the page fetches it and displays the data, i.e. user submitted content
 }
