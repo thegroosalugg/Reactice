e41387e26d25a327eb10a5bef7d1eead0bb4389b
victor loginov <tkoe88@zoho.com>
2024-03-20 19:54:34 +0100

    tested framer motion playground

--- 27-Animations/playground/src/App.jsx
+++ 27-Animations/playground/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { motion } from 'framer-motion'; // import from framer motion
 
 function App() {
   const [x, setX] = useState(0);
@@ -6,33 +7,42 @@ function App() {
   const [rotate, setRotate] = useState(0);
 
   return (
-    <div id="demo">
-      <div id="box" />
+    // convert HTML elements to frame motion
+    <div id='demo'>
+      <motion.div
+        id='box'
+        animate={{ x, y, rotate }} // takes object as value with x, y & rotate properties which match the state so this is short JS syntax
+        transition={{
+          duration: 0.1, // transition duration
+          bounce: 0.5, // set bounce between 0 and 1
+          type: 'spring', // configure animation type
+        }}
+      />
 
-      <div id="inputs">
+      <div id='inputs'>
         <p>
-          <label htmlFor="x">X</label>
+          <label htmlFor='x'>X</label>
           <input
-            type="number"
-            id="x"
+            type='number'
+            id='x'
             onChange={(event) => setX(+event.target.value)}
           />
         </p>
 
         <p>
-          <label htmlFor="y">Y</label>
+          <label htmlFor='y'>Y</label>
           <input
-            type="number"
-            id="y"
+            type='number'
+            id='y'
             onChange={(event) => setY(+event.target.value)}
           />
         </p>
 
         <p>
-          <label htmlFor="rotate">Rotate</label>
+          <label htmlFor='rotate'>Rotate</label>
           <input
-            type="number"
-            id="rotate"
+            type='number'
+            id='rotate'
             onChange={(event) => setRotate(+event.target.value)}
           />
         </p>
