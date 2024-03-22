b84af13e2ee6b8621373567c62c0ce66c5fd9431
victor loginov <tkoe88@zoho.com>
2024-03-22 19:14:02 +0100

    dummy content added

--- 28-ReactPatterns&BestPractices/src/App.jsx
+++ 28-ReactPatterns&BestPractices/src/App.jsx
@@ -1,5 +1,28 @@
+import Accordion from './components/accordion/Accordion';
+import AccordionItem from './components/accordion/AccordionItem';
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>React Patterns & Practices</h2>
+        <Accordion className='accordion'>
+        <AccordionItem className='accordion-item' title='Compound Components'>
+            <artic>
+              <p>Compound Components are designed to specifically work together</p>
+              <p>But they are not too useful on their own.</p>
+            </artic>
+          </AccordionItem>
+          <AccordionItem className='accordion-item' title='TBD'>
+            <artic>
+              <p>More Coming Soon...</p>
+              <p>...</p>
+            </artic>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
