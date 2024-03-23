3712b569977e3ed9ae36bfa5635240a6e26a4953
victor loginov <tkoe88@zoho.com>
2024-03-23 22:52:23 +0100

    added motion to nav bar

--- 28-ReactPatterns&BestPractices/src/components/NavBar/NavButton.jsx
+++ 28-ReactPatterns&BestPractices/src/components/NavBar/NavButton.jsx
@@ -1,11 +1,21 @@
 import { useNavContext } from './NavBar';
+import { motion } from 'framer-motion';
 
 export default function NavButton({ tab }) {
   const { activeTab, changeActiveTab } = useNavContext();
 
   return (
-    <li className={activeTab === tab ? 'active' : ''}>
-      <button onClick={() => changeActiveTab(tab)}>{tab}</button>
+    <li>
+      <motion.button
+        whileHover={{ scale: 1.1, color: '#0f86ef' }}
+        transition={{ type: 'easeInOut', duration: 0.5 }}
+        onClick={() => changeActiveTab(tab)}
+      >
+        {tab}
+      </motion.button>
+      {activeTab === tab && (
+        <motion.div layoutId='tab-indicator' className='active' />
+      )}
     </li>
   );
 }
