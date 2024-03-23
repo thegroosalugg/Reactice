70ee576bf25cc80887d6585a260a6c8f9f086d53
victor loginov <tkoe88@zoho.com>
2024-03-23 15:37:58 +0100

    update universe data component and styling

--- 28-ReactPatterns&BestPractices/src/components/Universe.jsx
+++ 28-ReactPatterns&BestPractices/src/components/Universe.jsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
+
 export default function Universe({ spaceRock }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleClick() {
+    setIsOpen((prevState) => !prevState);
+  }
+
   return (
-    <div>
-      {Object.keys(spaceRock).map((key, index) => (
-        <div key={index}>
-          <span>{key}: </span>
-          <span>{spaceRock[key]}</span>
-        </div>
-      ))}
+    <div className='space' onClick={handleClick}>
+      <header className={spaceRock.type}>
+        <h3>{spaceRock.name}</h3>
+        <h5>{spaceRock.type.toUpperCase()}</h5>
+      </header>
+      <article className={isOpen ? '' : 'hidden'}>
+        {Object.keys(spaceRock).map((key, index) => {
+          if (key !== 'name' && key !== 'type') {
+            return (
+              <div key={index} className='data'>
+                <span>
+                  <b>{key}: </b>{' '}
+                </span>
+                <span>{spaceRock[key]}</span>
+              </div>
+            );
+          }
+        })}
+      </article>
     </div>
   );
 }
