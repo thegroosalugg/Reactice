1cf28db7585dcec009b59176c93409b316bc7d55
victor loginov <tkoe88@zoho.com>
2024-03-24 18:52:12 +0100

    created search context

--- 28-ReactPatterns&BestPractices/src/components/SearchableList/SearchableList.jsx
+++ 28-ReactPatterns&BestPractices/src/components/SearchableList/SearchableList.jsx
@@ -1,11 +1,22 @@
-import { useRef, useState } from 'react';
+import { createContext, useContext, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 
 // Render props: React pattern where a component's functionality is provided by passing a function (the "render prop")
 // as a prop to the component. This function returns React elements that define what the component should render.
 
-export default function SearchableList({ flex, items, keyFn, label, children }) {
+const SearchContext = createContext({ searchResults: [] });
 
+export function useSearchContext() {
+  const ctx = useContext(SearchContext);
+
+  if (!ctx) {
+    throw new Error('useSearchContext must be wrapped by <SearchableList>');
+  }
+
+  return ctx;
+}
+
+export default function SearchableList({ flex, items, keyFn, label, children }) {
   const lastChange = useRef(); // store as a ref to presrve its value across state re-renders
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -16,52 +27,57 @@ export default function SearchableList({ flex, items, keyFn, label, children }) 
   function handleChange(event) {
     if (lastChange.current) {
       clearTimeout(lastChange.current); // would work the same without the if check, this runs everytime when there is a timer
-      console.log('Clearing Timeout', lastChange.current);
     }
 
     // function adds a delay before changing state after user input, to let the user to finish typing before searching
     lastChange.current = setTimeout(() => {
       lastChange.current = null; // would work without this. Clear Timeout already resets the timer
       setSearchTerm(event.target.value);
-      console.log('Expired', lastChange.current);
     }, 1500);
   }
 
+  const contextValue = {
+    searchTerm,
+    searchResults,
+  };
+
   return (
-    <div className='searchable-list'>
-      <motion.input
-        type='search'
-        placeholder={label}
-        onChange={handleChange}
-        initial={{ translateY: 100 }}
-        animate={{ translateY: 0 }}
-        transition={{ type: 'easeInOut', duration: 0.5 }}
-        whileFocus={{ scale: 1.1 }}
-      />
-      <motion.ul
-        className={flex ? 'space-list' : ''}
-        variants={{
-          visible: { opacity: 1, transition: { staggerChildren: 0.1 } },
-          hidden: { opacity: 0 },
-        }}
-        initial='hidden'
-        animate='visible'
-      >
-        {searchResults.map((item) => (
-          // pass a function as  a prop, so we can pass the found item to the parent for varying handling of keys
-          <motion.li
-            key={keyFn(item)}
-            variants={{
-              visible: { opacity: 1, scale: 1 },
-              hidden: { opacity: 0, scale: 1.2 },
-            }}
-          >
-            {/* here children expects to receive a function that returns renderable code.
+    <SearchContext.Provider value={contextValue}>
+      <div className='searchable-list'>
+        <motion.input
+          type='search'
+          placeholder={label}
+          onChange={handleChange}
+          initial={{ translateY: 100 }}
+          animate={{ translateY: 0 }}
+          transition={{ type: 'easeInOut', duration: 0.5 }}
+          whileFocus={{ scale: 1.1 }}
+        />
+        <motion.ul
+          className={flex ? 'space-list' : ''}
+          variants={{
+            visible: { opacity: 1, transition: { staggerChildren: 0.1 } },
+            hidden: { opacity: 0 },
+          }}
+          initial='hidden'
+          animate='visible'
+        >
+          {searchResults.map((item) => (
+            // pass a function as  a prop, so we can pass the found item to the parent for varying handling of keys
+            <motion.li
+              key={keyFn(item)}
+              variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 1.2 },
+              }}
+            >
+              {/* here children expects to receive a function that returns renderable code.
             This allows the child to pass data up to the parent, and for the parent to expect a parameter */}
-            {children(item)}
-          </motion.li>
-        ))}
-      </motion.ul>
-    </div>
+              {children(item)}
+            </motion.li>
+          ))}
+        </motion.ul>
+      </div>
+    </SearchContext.Provider>
   );
 }
--- 28-ReactPatterns&BestPractices/src/components/SolarSystem/SolarSystem.jsx
+++ 28-ReactPatterns&BestPractices/src/components/SolarSystem/SolarSystem.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import SpaceItem from './SpaceItem';
 import Modal from '../Modal';
+import { useSearchContext } from '../SearchableList/SearchableList';
 
 export default function SolarSystem({ item }) {
   const [isOpen, setIsOpen] = useState(false);
+  const { searchResults, searchTerm } = useSearchContext()
+
+  console.log('[searchResults]', searchResults, '\n', '[searchTerm]', searchTerm)
 
   function handleClick() {
     setIsOpen((prevState) => !prevState);
