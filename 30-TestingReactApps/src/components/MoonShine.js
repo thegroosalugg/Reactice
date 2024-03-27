331f5203972eed92c91009012f219b8fb94e3f41
victor loginov <tkoe88@zoho.com>
2024-03-27 19:59:30 +0100

    added further tests

--- 30-TestingReactApps/src/components/MoonShine.js
+++ 30-TestingReactApps/src/components/MoonShine.js
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 export default function MoonShine() {
+  const [clicked, setClicked] = useState(false);
+
+  function changeText() {
+    setClicked((prevState) => !prevState);
+  }
+
   return (
     <div>
       <h2>MoonShine</h2>
-      <p>Whisky River!</p>
+      {!clicked && <p>Bootlegging</p>}
+      {clicked && <p>Prohibition</p>}
+      <button onClick={changeText}>Mash</button>
     </div>
   );
 }
--- 30-TestingReactApps/src/components/MoonShine.test.js
+++ 30-TestingReactApps/src/components/MoonShine.test.js
@@ -1,16 +1,46 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import MoonShine from './MoonShine';
 
 // groups together related test cases. Used to create a test suite: collection of test cases that test the behavior of a specific component, module, or feature.
-describe('Test Suite', () => {
+describe('Test Suite <MoonShine />', () => {
   // test takes a description as 1st argument and a function as 2nd
-  test('This is a test, this is only, a test', () => {
+  test('01: renders "moonshine"', () => {
     render(<MoonShine />); // ARRANGE: Set up the necessary preconditions and inputs for the test.
 
     // ACT: Execute the specific behavior or functionality being tested - Not needed here
 
     // ASSERT: Verify that the expected outcomes of the behavior being tested have occurred
-    const quickTest = screen.getByText('whisky river', { exact: false }); // for greater flexebility use REGEXP: (/insert text/i)
-    expect(quickTest).toBeInTheDocument();
+    const textElement = screen.getByText('moonshine', { exact: false }); // for greater flexebility use REGEXP: (/insert text/i)
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test('02: renders "bootlegging", button NOT clicked', () => {
+    render(<MoonShine />);
+    const textElement = screen.getByText(/bootlegging/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test('03: renders "prohibition", button IS clicked', () => {
+    render(<MoonShine />); // ARRANGE
+
+    // ACT
+    const buttonElement = screen.getByRole('button'); // button element is a 'role'
+    userEvent.click(buttonElement); // simulate a user clicking on an element
+
+    const textElement = screen.getByText(/prohibition/i); // ASSERT
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test('04: does not render "bootlegging", button IS clicked', () => {
+    render(<MoonShine />);
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    // queryByText returns null if the element is not found, while getByText throws an error
+    const textElement = screen.queryByText(/bootlegging/i);
+    // expect(textElement).not.toBeInTheDocument();
+    expect(textElement).toBeNull();
   });
 });
