618f74a690222d6ad0ec8b851bc1fcfb1d6dd73e
victor loginov <tkoe88@zoho.com>
2024-03-30 23:57:31 +0100

    updated to do component + styling

--- 31-TypeScript/src/components/ToDoItem.tsx
+++ 31-TypeScript/src/components/ToDoItem.tsx
@@ -1,6 +1,11 @@
 // id key needs to be assigned in parent where this is called, so not used here
 const ToDo: React.FC<{ text: string }> = ({ text }) => {
-  return <li>{text}</li>;
+  return (
+    <li>
+      <p>{text}</p>
+      <button>Delete</button>
+    </li>
+  );
 };
 
 export default ToDo;
