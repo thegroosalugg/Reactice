7f7ab18f326b7ea95f66e3eee47e7655bc15faee
victor loginov <tkoe88@zoho.com>
2024-03-30 22:05:47 +0100

    can add todos via form and display them

--- 31-TypeScript/src/App.tsx
+++ 31-TypeScript/src/App.tsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import NewToDo from './components/NewToDo';
 import ToDos from './components/ToDos';
 import ToDo from './models/ToDo';
 
 function App() {
-  // when using classes we do not need to construct an object explicitly and can just pass required data
-  const todos = [new ToDo('Hello mother'), new ToDo('Hello father')];
+  // state type should be an array of ToDo class objects
+  const [todos, setTodos] = useState<ToDo[]>([]);
+
+  // function expects a string which is passed to the class object
+  const addToDoHandler = (text: string) => {
+    const newToDo = new ToDo(text);
+    setTodos((prevTodos) => [newToDo, ...prevTodos]);
+  };
 
   return (
     <main>
       <h2>Hello Victor, how are you today?</h2>
-      <NewToDo />
+      <NewToDo addToDo={addToDoHandler} />
       <ToDos items={todos} />
     </main>
   );
--- 31-TypeScript/src/components/NewToDo.tsx
+++ 31-TypeScript/src/components/NewToDo.tsx
@@ -1,21 +1,24 @@
 import { useRef, useState } from 'react';
 
-const NewToDo = () => {
+// FC type declared is a function that takes a  string as an argument and does not return any valud (void)
+const NewToDo: React.FC<{ addToDo: ( text: string ) => void }>  = ({ addToDo }) => {
   const inputText = useRef<HTMLInputElement>(null); // TS requires ref to select and HTML element for type and set an initial value
   const [error, setError] = useState<string>(); // state type set to string
+
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
     // in TS, current is followed by ? when the value could be null, or ! if the value is guaranteed
-    const userData = inputText.current!.value;
+    const formData = inputText.current!.value;
 
-    if (userData.trim().length === 0) {
+    if (formData.trim().length === 0) {
       setError("Field can't be empty");
       return;
     }
 
     setError('');
+    addToDo(formData);
   };
 
   return (
